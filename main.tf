bef70ab7d193f7c76b116749c66581d8eeac2449
shankar0203 <79186682+shankar0203@users.noreply.github.com>
2025-04-19 12:16:24 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_key_pair" "my_key" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
 resource "aws_security_group" "ssh_access" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
@@ -15,7 +10,7 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ⚠️ For production, restrict this!
+    cidr_blocks = ["0.0.0.0/0"] # ⚠️ Restrict in production
   }
 
   egress {
@@ -29,7 +24,7 @@ resource "aws_security_group" "ssh_access" {
 resource "aws_instance" "ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.my_key.key_name
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
   tags = {
